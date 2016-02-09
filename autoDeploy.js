6a8adec289adcd34d3348bfb28d5526801439144
jasine <jasinechen@gmail.com>
2016-02-10 01:41:43 +0800

    add hooks to site

--- autoDeploy.js
+++ autoDeploy.js
@@ -1,37 +1,44 @@
 var http = require('http')
 var createHandler = require('github-webhook-handler')
 var handler = createHandler({ path: '/incoming/site', secret: '12345678' })
+var process = require('child_process');
+var fs=require('fs');
+
+if(!fs.existsSync("./public")){
+  console.log("!!!");
+  pullAndGen();
+}
 
 http.createServer(function (req, res) {
-  console.log(req);
   handler(req, res, function (err) {
     console.log(err);
     res.statusCode = 404
     res.end('no such location')
   })
 }).listen(8086)
+console.log('github hook listening...');
+handler.on('error', function (err) {
+  console.error('Error:', err.message)
+})
 
-// handler.on('error', function (err) {
-//   console.error('Error:', err.message)
-// })
-
-// handler.on('push', function (event) {
-//   console.log('Received a push event for %s to %s',
-//     event.payload.repository.name,
-//     event.payload.ref)
-// })
-
-// handler.on('issues', function (event) {
-//   console.log('Received an issue event for %s action=%s: #%d %s',
-//     event.payload.repository.name,
-//     event.payload.action,
-//     event.payload.issue.number,
-//     event.payload.issue.title)
-// })
-   
-handler.on('*', function (event) {
-  console.log('Received an  event for %s action=%s',
+handler.on('push', function (event) {
+  console.log('Received a push event for %s at %s',
     event.payload.repository.name,
-    event.payload.action)
+    Date.now());
+    pullAndGen();
     
 })
+
+function pullAndGen(){
+  var cmd=process.exec("git pull && hexo generate");
+    cmd.on('err',function(err){
+      console.log(err);
+    });
+    cmd.on('exit', function(code) {
+      if (code != 0) {
+        console.log('Failed: ' + code);
+      }else{
+        console.log("deployed!");
+      }
+    });
+}
